e544d8190921b7f3df3a2cb49ea7da98f6cb0c31
Russell DuBrock <russell.dubrock@gmail.com>
2015-01-07 09:13:56 -0800

    Finished version of the third challenge in "Fundamentals" section

---
+++ jsClasswork/subtracttext2.js
@@ -0,0 +1,15 @@
+var number1 = 10
+var number2 = 5
+
+var differenceNumbers = function(x, y) {
+	var z = x - y
+	if (z < 0) {
+		z = -z
+	};
+	console.log("the difference between " + number1 + " and " + number2 + " equals " + z)	
+};
+
+ 
+differenceNumbers(number1, number2)
+
+
