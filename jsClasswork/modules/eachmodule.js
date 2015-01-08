c05c7037ad7e21f0883c0640f017e8f197d7a359
Russell DuBrock <russell.dubrock@gmail.com>
2015-01-08 13:08:39 -0800

    Finished each loop module exercises

--- jsClasswork/modules/eachmodule.js
+++ jsClasswork/modules/eachmodule.js
@@ -9,6 +9,29 @@
 // 	return array;
 // };
 
+module.exports = {
+	each: function (array, callback) {
+	//accepts an array and adds one to each item
+	array.forEach (function(v, i) {
+		array[i] = callback (array[i]);
+	});
+
+	return array;
+  },
+
+    sayHello: function() {
+	return "hello world";
+  },
+
+    version: function(version) {
+	version = "Version 0.1.0";
+
+	return version;
+
+  }
+};  
+
+
 module.exports.each = function (array, callback) {
 	//accepts an array and adds one to each item
 	array.forEach (function(v, i) {
